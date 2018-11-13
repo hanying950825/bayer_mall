f585beac36ab941752d1dcdefa8fc4d0ca24b0c2
hanying950825 <1171488600@qq.com>
2018-11-13 09:37:16 +0800

    首页

--- pages/home/home.js
+++ pages/home/home.js
@@ -5,8 +5,13 @@ Page({
 	 * 页面的初始数据
 	 */
 	data: {
-
-	},
+    imgUrls: [
+      'http://img02.tooopen.com/images/20150928/tooopen_sy_143912755726.jpg',
+      'http://img06.tooopen.com/images/20160818/tooopen_sy_175866434296.jpg',
+      'http://img06.tooopen.com/images/20160818/tooopen_sy_175833047715.jpg'
+    ],
+    imageURL: 'http://img06.tooopen.com/images/20160818/tooopen_sy_175833047715.jpg'
+  },
 
 	/**
 	 * 生命周期函数--监听页面加载
