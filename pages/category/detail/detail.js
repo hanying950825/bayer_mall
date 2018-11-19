8323a12ab8bc3336b9ee104ff44e9d47c2b59e5d
tayler1022 <tanglei@konyo.com.cn>
2018-11-19 14:14:33 +0800

    更新购物车分类等页面

---
+++ pages/category/detail/detail.js
@@ -0,0 +1,93 @@
+// pages/category/detail/detail.js
+Page({
+
+	/**
+	 * 页面的初始数据
+	 */
+	data: {
+		indicatorDots: true,
+		autoplay: true,
+		interval: 2500,
+		duration: 300,
+		imgUrls: [
+			'http://img02.tooopen.com/images/20150928/tooopen_sy_143912755726.jpg',
+			'http://img06.tooopen.com/images/20160818/tooopen_sy_175866434296.jpg',
+			'http://img06.tooopen.com/images/20160818/tooopen_sy_175833047715.jpg',
+			'http://img02.tooopen.com/images/20150928/tooopen_sy_143912755726.jpg'
+		],
+		goodsTitle: '这个是来自珠海的傻屌这个是来自珠海的傻屌这个是来自珠海的傻屌',
+		goodsPrice : 5000,
+		goodsPrivilegePrice: 8000,
+		goodsDetailImg:[
+			'http://img02.tooopen.com/images/20150928/tooopen_sy_143912755726.jpg',
+			'http://img06.tooopen.com/images/20160818/tooopen_sy_175866434296.jpg',
+			'http://img06.tooopen.com/images/20160818/tooopen_sy_175833047715.jpg',
+			'http://img02.tooopen.com/images/20150928/tooopen_sy_143912755726.jpg'
+		],
+		info: 5
+	},
+	//预览图片
+  previewImage: function (e) {
+    var current = e.target.dataset.src;
+    wx.previewImage({
+      current: current, // 当前显示图片的http链接  
+      urls: this.data.imgUrls // 需要预览的图片http链接列表  
+    })
+  },
+
+	/**
+	 * 生命周期函数--监听页面加载
+	 */
+	onLoad: function (options) {
+
+	},
+
+	/**
+	 * 生命周期函数--监听页面初次渲染完成
+	 */
+	onReady: function () {
+
+	},
+
+	/**
+	 * 生命周期函数--监听页面显示
+	 */
+	onShow: function () {
+
+	},
+
+	/**
+	 * 生命周期函数--监听页面隐藏
+	 */
+	onHide: function () {
+
+	},
+
+	/**
+	 * 生命周期函数--监听页面卸载
+	 */
+	onUnload: function () {
+
+	},
+
+	/**
+	 * 页面相关事件处理函数--监听用户下拉动作
+	 */
+	onPullDownRefresh: function () {
+
+	},
+
+	/**
+	 * 页面上拉触底事件的处理函数
+	 */
+	onReachBottom: function () {
+
+	},
+
+	/**
+	 * 用户点击右上角分享
+	 */
+	onShareAppMessage: function () {
+
+	}
+})
--- pages/home/home.js
+++ pages/home/home.js
@@ -7,10 +7,10 @@ Page({
 	data: {
     imgUrls: [
       'http://img02.tooopen.com/images/20150928/tooopen_sy_143912755726.jpg',
-      'http://img06.tooopen.com/images/20160818/tooopen_sy_175866434296.jpg',
-      'http://img06.tooopen.com/images/20160818/tooopen_sy_175833047715.jpg'
+      'http://img02.tooopen.com/images/20150928/tooopen_sy_143912755726.jpg',
+      'http://img02.tooopen.com/images/20150928/tooopen_sy_143912755726.jpg'
     ],
-    imageURL: 'http://img06.tooopen.com/images/20160818/tooopen_sy_175833047715.jpg'
+		imageURL: 'http://img02.tooopen.com/images/20150928/tooopen_sy_143912755726.jpg'
   },
 
 	/**
